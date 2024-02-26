2036ca021cb331d5b50a0ba3d3a57295227fec75
Beepul <79615064+Beepul@users.noreply.github.com>
2024-02-26 23:59:37 +0545

    display all restaurant with pagination, filter and sort

---
+++ frontend/src/components/SearchResultInfo.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+type Props = {
+  total: number;
+  city: string;
+}
+
+const SearchResultInfo = ({total, city}: Props) => {
+    return (
+        <div className="text-xl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row">
+            <span>
+                {total} Restaurants found in <span className="capitalize mr-2">{city}</span>
+                <Link to={'/'} className="text-sm font-semibold underline cursor-pointer text-blue-500">Change Location</Link>
+            </span>
+        </div>
+    )
+}
+
+export default SearchResultInfo;
