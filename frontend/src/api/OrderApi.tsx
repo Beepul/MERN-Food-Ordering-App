b7e9b92d8331477e9a6bfd81df80479a9c9ade63
Beepul <79615064+Beepul@users.noreply.github.com>
2024-03-01 23:42:34 +0545

    my order status page header

--- frontend/src/api/OrderApi.tsx
+++ frontend/src/api/OrderApi.tsx
@@ -1,8 +1,35 @@
+import { Order } from "@/types"
 import { useAuth0 } from "@auth0/auth0-react"
-import { useMutation } from "react-query"
+import { useMutation, useQuery } from "react-query"
 import { toast } from "sonner"
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+export const useGetMyOrders = () => {
+    const { getAccessTokenSilently } = useAuth0()
+
+    const getMyOrderRequset = async (): Promise<Order[]> => {
+        const accessToken = await getAccessTokenSilently()
+
+        const res = await fetch(`${API_BASE_URL}/api/order`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+
+        if(!res.ok) throw new Error('Failed to get orders')
+
+        return res.json()
+    }
+
+    const {data: orders, isLoading} = useQuery('fetchMyOrders', getMyOrderRequset)
+
+    return {
+        orders,
+        isLoading
+    }
+
+}
 
 type CheckoutSessionRequest = {
     cartItems: {
